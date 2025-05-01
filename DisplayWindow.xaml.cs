30033e6ad407d7d97018a34fa33c3422b2eafee7
pwrpc23 <23pwrpc23@gmail.com>
2025-05-01 18:11:15 +0300

    Add project files.

---
+++ DisplayWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Timer {
+	/// <summary>
+	/// Interaction logic for DisplayWindow.xaml
+	/// </summary>
+	public partial class DisplayWindow : Window {
+		public event EventHandler DisplayWindowClosed;
+		private bool showTimeText = true; // metin mi gösteriliyor, yoksa "SÜRE DOLDU" mu
+
+		public DisplayWindow() {
+			InitializeComponent();
+			this.LocationChanged += DisplayWindow_LocationChanged;
+			this.Closed += DisplayWindow_Closed; // pencere kapandığında çalışır
+		}
+
+		private void DisplayWindow_Closed(object sender, EventArgs e) {
+			DisplayWindowClosed?.Invoke(this, EventArgs.Empty);  // MainWindow’a haber gönder
+		}
+
+		public void UpdateTime(string time, int remainingSeconds) {
+			if(remainingSeconds >= 0) {
+				TimeLabel.Text = time;
+
+				if(remainingSeconds <= 120) // kırmızı
+					TimeLabel.Foreground = new SolidColorBrush(Colors.Red);
+				else if(remainingSeconds <= 180) // turuncu
+					TimeLabel.Foreground = new SolidColorBrush(Colors.Orange);
+				else
+					TimeLabel.Foreground = new SolidColorBrush(Colors.Green);
+			}
+			else {
+				// Süre dolduktan sonra: yanıp sönen davranış
+				if(showTimeText) {
+					TimeLabel.Text = time;  // süreyi göster
+				}
+				else {
+					TimeLabel.Text = "SÜRE DOLDU";  // uyarı metni
+				}
+
+				TimeLabel.Foreground = new SolidColorBrush(Colors.Red);  // kırmızı
+				showTimeText = !showTimeText;  // her tick'te değiştir
+			}
+		}
+
+		private void DisplayWindow_LocationChanged(object? sender, EventArgs e) {
+			var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+			var screen = Screen.FromHandle(hwnd);
+
+			var workingArea = screen.WorkingArea;  // bu ekranın kullanılabilir alanı (taskbar hariç)
+
+			double maxLeft = workingArea.Right - this.Width;
+			double maxTop = workingArea.Bottom - this.Height;
+
+			if(this.Left < workingArea.Left)
+				this.Left = workingArea.Left;
+			else if(this.Left > maxLeft)
+				this.Left = maxLeft;
+
+			if(this.Top < workingArea.Top)
+				this.Top = workingArea.Top;
+			else if(this.Top > maxTop)
+				this.Top = maxTop;
+		}
+
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+			this.DragMove();
+		}
+	}
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System;
+using System.Windows.Threading;
+using Timer;
+
+namespace Timer {
+	public partial class MainWindow : Window {
+		private int totalSeconds;
+		private DispatcherTimer timer;
+		private DisplayWindow displayWindow;
+
+		public MainWindow() {
+			InitializeComponent();
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += Timer_Tick;
+		}
+
+		private void Start_Click(object sender, RoutedEventArgs e) {
+			int minutes = MinutesUpDown.Value ?? 0;
+			int seconds = SecondsUpDown.Value ?? 0;
+
+			totalSeconds = minutes * 60 + seconds;
+
+			if(displayWindow == null || !displayWindow.IsLoaded) {
+				displayWindow = new DisplayWindow();
+				displayWindow.Owner = this;
+				displayWindow.Left = 0;
+				displayWindow.Top = 0;
+				displayWindow.Show();
+				displayWindow.DisplayWindowClosed += DisplayWindow_DisplayWindowClosed;
+			}
+
+			UpdateDisplay();
+			timer.Start();
+		}
+
+		private void DisplayWindow_DisplayWindowClosed(object? sender, EventArgs e) {
+			timer.Stop();  // DisplayWindow kapatıldığında timer da dursun
+		}
+
+		private void Pause_Click(object sender, RoutedEventArgs e) {
+			timer.Stop();
+		}
+
+		private void Resume_Click(object sender, RoutedEventArgs e) {
+			timer.Start();
+		}
+
+		private void Reset_Click(object sender, RoutedEventArgs e) {
+			timer.Stop();
+			totalSeconds = 0;
+			UpdateDisplay();
+		}
+
+		private void CloseDisplay_Click(object sender, RoutedEventArgs e) {
+			if(displayWindow != null && displayWindow.IsLoaded) {
+				displayWindow.Close();
+			}
+			timer.Stop();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e) {
+			totalSeconds--;
+			UpdateDisplay();
+		}
+
+		private void UpdateDisplay() {
+			int mins = totalSeconds / 60;
+			int secs = Math.Abs(totalSeconds % 60); // negatifte de pozitif gösterim
+			string sign = totalSeconds < 0 ? "-" : "";
+			string timeStr = $"{sign}{Math.Abs(mins):D2}:{secs:D2}";
+
+			if(displayWindow != null && displayWindow.IsLoaded)
+				displayWindow.UpdateTime(timeStr, totalSeconds);
+		}
+	}
+}
